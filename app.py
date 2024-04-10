5e40f767a2b116acf269b7bf74268abbcb1b4c6e
Alan Zavala <alan.j.zavala@gmail.com>
2024-04-10 14:37:21 -0500

    fixed login authenitcatin

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
-from flask import Flask, render_template, session, request, redirect, url_for
+from flask import Flask, render_template, session, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from sqlalchemy.sql import text
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
 
 app = Flask(__name__, template_folder='templates')
 db = SQLAlchemy(app)
@@ -14,7 +12,6 @@ bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secretkey'
-db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -24,16 +21,20 @@ login_manager.login_view = "login"
 def load_user(user_id):
      return User.query.get(int(user_id))
 
-def clear_data(session):
-    meta = db.metadata
-    for table in reversed(meta.sorted_tables):
-        session.execute(table.delete())
-    session.commit()
+# def clear_data(session):
+#     meta = db.metadata
+#     for table in reversed(meta.sorted_tables):
+#         session.execute(table.delete())
+#     session.commit()
 
 class User(db.Model, UserMixin):
      id = db.Column(db.Integer, primary_key = True)
-     username = db.Column(db.String(40), nullable=False, unique=True)
-     password = db.Column(db.String(40), nullable=False)
+     username = db.Column(db.String(20), nullable=False, unique=True)
+     password = db.Column(db.String(80), nullable=False)
+
+     def __init__(self, username, password):
+          self.usename = username
+          self.password = bcrypt.generate_password_hash(password)
 
 class RegisterForm(FlaskForm):
      username = StringField(validators=[InputRequired(), Length(
@@ -70,7 +71,7 @@ def testdb():
 
         user_text = '<ul>'
         for name in users:
-            user_text += '<li>' + name.username + '</li>'
+            user_text += '<li>' + name.username + ',' + name.password + '</li>'
         user_text += '</ul>'
         return user_text
     except Exception as e:
@@ -89,9 +90,11 @@ def login():
      if form.validate_on_submit():
           user = User.query.filter_by(username=form.username.data).first()
           if user:
-               if bcrypt.check_password_hash(user.password, form.password.data):
+               if check_password_hash(user.password, form.password.data):
                     login_user(user)
                     return redirect(url_for('dashboard'))
+          else:
+               flash('Invalid username or password', 'error')
      return render_template('login.html', form=form)
 
 @app.route('/dashboard', methods=['GET', 'POST'])
@@ -104,22 +107,6 @@ def dashboard():
 def logout():
      logout_user()
      return redirect(url_for('login'))
-
-#   ------ unnecessary function ------
-#
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#      form = RegisterForm
-     
-#      if form.validate_on_submit():
-#           hashed_password = bcrypt.generate_passowrd_hash(form.passowrd.data)
-#           new_user = User(username=form.username.data, password=hashed_password)
-#           db.session.add(new_user)
-#           db.session.commit()
-#           return redirect(url_for('login'))
-     
-#      return render_template('register.html', form=form)
-
 
 if __name__ == '__main__':
      app.run(debug=True)
